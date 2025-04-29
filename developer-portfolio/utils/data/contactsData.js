0b336b67ee21e9dd24447567ac271fae72e22f37
abhijith T A <abhijithajith13@gmail.com>
2025-04-29 22:42:20 +0530

    first commit

---
+++ developer-portfolio/utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'ajaychekavarck@gmail.com',
+    phone: '+91-9544643754',
+    address: 'Tirur, Kerala, India',
+    github: 'https://github.com/Ajay-ck',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://linkedin.com/in/ajay-ck',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
